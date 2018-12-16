b42fdd2fdfb2797ff8127882cb6cc0eeeefb62fa
Philipp <philippkostyra@gmail.com>
2018-12-16 16:57:17 +0100

    Test for Calendar Main Router

--- test/spec/router/CalendarMainRouter.spec.ts
+++ test/spec/router/CalendarMainRouter.spec.ts
@@ -7,14 +7,15 @@ import { expect } from "chai";
 
 chai.use(chaiHttp);
 
-describe("AppointmentRouter", async () => {
-  describe("GET Appointement", () => {
+describe("Calendar Main Router", async () => {
+  describe("GET", () => {
     it("should run HTTP Status 200", async () => {
       return chai
         .request(app)
         .get("/")
         .then(res => {
           console.log(res);
+          expect(res.status).is.eq(200);
         });
     });
   });
